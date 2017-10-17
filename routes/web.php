6f60ee170900ebcd68b49cdff9497a66a071cadb
Dmytro <dimka82132@gmail.com>
2017-10-17 09:13:09 +0300

    Route for main page

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,9 @@
 
 Route::group([], function () {
 
-    Route::match(['get', 'post'], function () {
+    Route::match(['get', 'post'], '/', ['uses' => 'IndexController@execute', 'as' => 'home']);
 
-    });
+    Route::get('/page/{alias}', ['uses' => 'PageController@execute', 'as' => 'page']);
 
+    Route::auth();
 });
