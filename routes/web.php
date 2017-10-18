aa6a55afa733617df29d2d113e63f91e1080c14b
Dmytro <dimka82132@gmail.com>
2017-10-18 11:09:04 +0300

    Route for admin page

--- routes/web.php
+++ routes/web.php
@@ -24,10 +24,44 @@ Route::group([], function () {
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
 
+    //admin
     Route::get('/', function () {
 
-
-
     });
+
+
+    //admin/pages
+    Route::group(['prefix' => 'pages'], function () {
+
+        //admin/pages
+        Route::get('/', ['uses' => 'PagesController@execute', 'as' => 'pages']);
+        //admin/pages/add
+        Route::match(['get', 'post'], '/add', ['uses' => 'PagesAddController@execute', 'as' => 'pagesAdd']);
+        //admin/edit/2
+        Route::match(['get', 'post', 'delete'], '/edit/{page}', ['uses' => 'PagesEditController@execute', 'as' => 'pagesEdit']);
+    });
+
+
+    Route::group(['prefix' => 'portfolio'], function () {
+
+        //admin/potfolio
+        Route::get('/', ['uses' => 'PortfolioController@execute', 'as' => 'portfolio']);
+        //admin/potfolio/add
+        Route::match(['get', 'post'], '/add', ['uses' => 'PortfolioAddController@execute', 'as' => 'portfolioAdd']);
+        //admin/edit/2
+        Route::match(['get', 'post', 'delete'], '/edit/{portfolio}', ['uses' => 'PortfolioEditController@execute', 'as' => 'portfolioEdit']);
+    });
+
+
+    Route::group(['prefix' => 'services'], function () {
+
+        //admin/services
+        Route::get('/', ['uses' => 'ServiceController@execute', 'as' => 'services']);
+        //admin/services/add
+        Route::match(['get', 'post'], '/add', ['uses' => 'ServiceAddController@execute', 'as' => 'serviceAdd']);
+        //admin/edit/2
+        Route::match(['get', 'post', 'delete'], '/edit/{service}', ['uses' => 'ServiceEditController@execute', 'as' => 'serviceEdit']);
+    });
+
 
 });
