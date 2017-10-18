8f50b2ba5e3e3d8da82d7f87c0ea4b7294c0c11d
Dmytro <dimka82132@gmail.com>
2017-10-18 13:54:25 +0300

    Create models and use blade for main page

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,14 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\{
+    Page,
+    Service,
+    Portfolio,
+    People
+};
 
 class IndexController extends Controller
 {
     //
     public function execute(Request $request)
     {
+        $pages = Page::all();
+        $peoples = People::all();
+        $portfolios = Portfolio::all();
+        $service = Service::all();
 
-        return view('layouts/site');
+
+        return view('site.index');
 
     }
 }
